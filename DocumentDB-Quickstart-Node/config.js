6fd47106a7e556b7af34db8804ca0a43cc5e96d2
sbt_kotakahashi <kotakahashi@tech.softbank.co.jp>
2018-02-19 03:34:55 +0900

    remove trash project

--- DocumentDB-Quickstart-Node/config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.host = process.env.HOST || "https://ecgeojsonsample.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "FHjYMtqWxw2sXZaGQ5oug8tKoPMnubvDyfpoL08nTjcbSflsLHnNsnb9SbZX0OM7A491rWc42eVNUk2lG5oDpA==";
-config.databaseId = "genkyo";
-config.collectionId = "genkyo";
-
-module.exports = config;
--- FuncShapeTpGeoJSON/ShapeToDocumentDB/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (context, myBlob) {
-    context.log(
-        "JavaScript blob trigger function processed blob \n Name:", context.bindingData.name,
-         "\n Blob Size:", myBlob.length, "Bytes");
-    context.bindings.myOutputBlob = context.bindings.myInputBlob;
-    context.done();
-};
